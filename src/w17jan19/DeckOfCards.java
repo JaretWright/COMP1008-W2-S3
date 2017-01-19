6b51b95ce17840b9a2e04337fd95174804e0705f
JWright <JWright@192.168.56.1>
2017-01-19 15:23:42 -0500

    Added initial code

---
+++ src/w17jan19/DeckOfCards.java
@@ -0,0 +1,64 @@
+package w17jan19;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author jwright
+ */
+public class DeckOfCards {
+    private ArrayList<Card> deck;
+    
+    public DeckOfCards()
+    {
+        deck = new ArrayList<>();
+        
+        String[] suits = {"spades","hearts","clubs","diamonds"};
+        String[] faceNames = {"two","three","four","five","six","seven","eight",
+                              "nine","ten","Jack","Queen","King","Ace"};
+        
+        for (String suit : suits)
+        {
+            for (int i=0; i < faceNames.length; i++)
+            {
+                Card newCard = new Card(faceNames[i], suit, i+2);
+                deck.add(newCard);
+            }
+        }
+    }
+    
+    /**
+     * This shuffles the deck of cards
+     */
+    public void shuffle()
+    {
+        Collections.shuffle(deck);
+    }
+    
+    /**
+     * This method will "deal" the top card off the deck
+     * and return it to the calling function
+     * @return 
+     */
+    public Card dealTopCard()
+    {
+        if (deck.size() > 0)
+            return deck.remove(0);
+        else
+            return null;
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        String deckString="";
+        for (Card card : deck)
+        {
+            deckString += card.toString() + "\n";
+        }
+        return deckString;
+    }
+
+}
---
+++ src/w17jan19/W17Jan19.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w17jan19;
+
+/**
+ *
+ * @author JWright
+ */
+public class W17Jan19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameOfWar theGame = new GameOfWar();
+        theGame.playTheGame();
+    }
+    
+}
